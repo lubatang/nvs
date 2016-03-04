08ccd1487caeccbf8c7f23e6cc4946be92843801
Skymizer <lubatang@gmail.com>
2016-03-05 07:14:16 +0800

    Add SVNClient.

---
+++ src/SVNClient.h
@@ -0,0 +1,23 @@
+//===----------------------------------------------------------------------===//
+//
+//                            The NVS Project
+//
+// This file is distributed under GNU GENERAL PUBLIC LICENSE, Version 3.
+// See LICENSE for details.
+//
+//===----------------------------------------------------------------------===//
+#ifndef NVS_SUBVERSION_CLIENT_H
+#define NVS_SUBVERSION_CLIENT_H
+
+namespace nvs {
+
+/** \class SVNClient
+ *  \brief provides facade to libsvn_client.
+ */
+class SVNClient
+{
+};
+
+} // namespace of nvs
+
+#endif
