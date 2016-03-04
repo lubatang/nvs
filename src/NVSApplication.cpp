05779611eadf5ef4e38beae90af673d3ab63e3fa
Skymizer <lubatang@gmail.com>
2016-03-05 03:26:33 +0800

    Add scaffolding ofr NVSApplication.

---
+++ src/NVSApplication.cpp
@@ -0,0 +1,22 @@
+//===----------------------------------------------------------------------===//
+//
+//                            The NVS Project
+//
+// This file is distributed under GNU GENERAL PUBLIC LICENSE, Version 3.
+// See LICENSE for details.
+//
+//===----------------------------------------------------------------------===//
+#include <NVSApplication.h>
+
+using namespace nvs;
+
+//===----------------------------------------------------------------------===//
+// NVSApplication
+//===----------------------------------------------------------------------===//
+NVSApplication::NVSApplication()
+{
+}
+
+NVSApplication::~NVSApplication()
+{
+}
---
+++ src/NVSApplication.h
@@ -0,0 +1,27 @@
+//===----------------------------------------------------------------------===//
+//
+//                            The NVS Project
+//
+// This file is distributed under GNU GENERAL PUBLIC LICENSE, Version 3.
+// See LICENSE for details.
+//
+//===----------------------------------------------------------------------===//
+#ifndef NVS_APPLICATION_H
+#define NVS_APPLICATION_H
+
+namespace nvs {
+
+/** \class NVSApplication
+ *  \brief provides the application for MVC pattern.
+ */
+class NVSApplication
+{
+public:
+  NVSApplication();
+
+  ~NVSApplication();
+};
+
+} // namespace of nvs
+
+#endif
