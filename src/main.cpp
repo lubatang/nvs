31be6e118ca5e48d17ad5aa4586182344502407f
Skymizer <lubatang@gmail.com>
2016-03-05 03:11:13 +0800

    Add the main file.

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+//===----------------------------------------------------------------------===//
+//
+//                            The NVS Project
+//
+// This file is distributed under GNU GENERAL PUBLIC LICENSE, Version 3.
+// See LICENSE for details.
+//
+//===----------------------------------------------------------------------===//
+
+int main(int pArgc, char* pArgv[])
+{
+}
