a29d102055c8896b83a5121ec360bb7688fb62dc
Skymizer <lubatang@gmail.com>
2016-03-13 15:27:54 +0800

    Remove Object.

--- include/Widget/Layout.cpp
+++ include/Widget/Layout.cpp
@@ -7,14 +7,59 @@
 //
 //===----------------------------------------------------------------------===//
 #include <Widget/Layout.h>
+#include <Widget/Widget.h>
+#include <curses.h>
 #include <assert.h>
 
 using namespace nvs;
 
+extern WINDOW* stdscr;
+
 //===----------------------------------------------------------------------===//
 // Layout
 //===----------------------------------------------------------------------===//
-Layout::Layout(Widget* pParent)
-  : Object(pParent), m_pParent(pParent) {
-  assert(nullptr != pParent && "Layout must have a parent!");
+Layout::Layout(Orientations pDirection, Widget* pParent)
+  : m_pWindow(nullptr), m_Direction(pDirection) {
+  if (nullptr == pParent) { //< top-level layout
+    Widget::setLayout(this);
+    m_pWindow = stdscr;
+  }
+  else { //< get the size from the parent
+    m_pWindow = newwin(pParent->height(), pParent->width(),
+                       pParent->y(), pParent->x());
+  }
+}
+
+Layout::~Layout()
+{
+  if (stdscr != m_pWindow)
+    delwin(m_pWindow);
+}
+
+// re-calculate the size of the layout and all widgets.
+void Layout::addWidget(Widget& pWidget)
+{
+  m_Components.push_back(&pWidget);
+  this->resizeComponents();
+}
+
+//===----------------------------------------------------------------------===//
+// HLayout
+//===----------------------------------------------------------------------===//
+HLayout::HLayout(Widget* pParent)
+  : Layout(Layout::Horizontal, pParent) {
+}
+
+void HLayout::resizeComponents()
+{
+  int x, y, width, height;
+  getmaxyx(m_pWindow, height, width);
+  getbegyx(m_pWindow, y, x);
+
+  int tile = width / m_Components.size();
+  int c = 0;
+  ComponentList::iterator widget, wEnd = m_Components.end();
+  for (widget = m_Components.begin(); widget != wEnd; ++widget, ++c) {
+    // widget->updateWindow(m_pWindow, x + tile*c, y, tile*c, height)
+  }
 }
--- include/Widget/Layout.h
+++ include/Widget/Layout.h
@@ -8,20 +8,57 @@
 //===----------------------------------------------------------------------===//
 #ifndef NVS_WIDGET_LAYOUT_H
 #define NVS_WIDGET_LAYOUT_H
-#include <Widget/Widget.h>
 #include <vector>
+#include <curses.h>
 
 namespace nvs {
 
-class Layout : public Object
+class Widget;
+
+/** \class Layout
+ *  \brief The Layout class is the base class of geometry managers
+ *
+ *  A Layout object creates a new WINDOW object.
+ */
+class Layout
 {
 public:
-  explicit Layout(Widget* pParent);
+  enum Orientations {
+    Horizontal,
+    Vertical
+  };
 
-  Widget* parent() const { return m_pParent; }
+public:
+  /// @param[in,out] pParent The parent widget. If the parent widget is null,
+  /// then this is a top-level layout.
+  /// construct a new top-level layout
+  Layout(Orientations pDirection, Widget* pParent = nullptr);
+
+  virtual ~Layout();
+
+  Orientations direction() const { return m_Direction; }
+
+  /// add widget into layout.
+  void addWidget(Widget& pWidget);
 
 protected:
-  Widget* m_pParent;
+  typedef std::vector<Widget*> ComponentList;
+
+  virtual void resizeComponents() = 0;
+
+protected:
+  WINDOW* m_pWindow;
+  Orientations m_Direction;
+  ComponentList m_Components;
+};
+
+class HLayout : public Layout
+{
+public:
+  HLayout(Widget* pParent = nullptr);
+
+private:
+  void resizeComponents();
 };
 
 } // namespace of nvs
