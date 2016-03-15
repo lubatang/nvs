8a11d05ca6ac4d1dea7bd7680d32c83e0a2b47ad
Skymizer <lubatang@gmail.com>
2016-03-16 03:57:14 +0800

    Add MainWindow.

---
+++ include/Widget/MainWindow.h
@@ -0,0 +1,21 @@
+//===----------------------------------------------------------------------===//
+//
+//                            The NVS Project
+//
+// This file is distributed under GNU GENERAL PUBLIC LICENSE, Version 3.
+// See LICENSE for details.
+//
+//===----------------------------------------------------------------------===//
+#ifndef NVS_WIDGET_MAINWINDOW_H
+#define NVS_WIDGET_MAINWINDOW_H
+#include <Widget/Widget.h>
+
+namespace nvs {
+
+class MainWindow : public Widget
+{
+};
+
+} // namespace nvs
+
+#endif
